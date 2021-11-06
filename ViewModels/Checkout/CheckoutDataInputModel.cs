921cef45a67c7483c011afaf4e08e8c33d80b2c3
Sasha <sanyokkotenko2002@gmail.com>
2021-11-06 17:47:35 +0200

    Checkout step

---
+++ ViewModels/Checkout/CheckoutDataInputModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebShop.ViewModels.Client;
+
+namespace WebShop.ViewModels.Checkout
+{
+    public class CheckoutDataInputModel
+    {
+        public Address ClientAddress { get; set; }
+
+        public ClientData ClientData { get; set; }
+
+        public int DeliveryId { get; set; }
+
+        public int BasketId { get; set; }
+    }
+}
---
+++ ViewModels/Client/ClientData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShop.ViewModels.Client
+{
+    public class ClientData
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
